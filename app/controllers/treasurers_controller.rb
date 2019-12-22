6b34b6fad405e1767f78fe421a7dc99ed7b01df2
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2019-12-22 01:06:51 -0500

    login issues

--- app/controllers/treasurers_controller.rb
+++ app/controllers/treasurers_controller.rb
@@ -98,7 +98,7 @@ class TreasurersController < ApplicationController
     end
 	
 		def check_admin_access
-		  redirect_to current_user.treasurer if current_user.treasurer.present?
+		  redirect_to current_user.treasurer if (current_user.treasurer?)
 			redirect_to new_treasurer_path if current_user.treasurer.blank?
 	  end
 	

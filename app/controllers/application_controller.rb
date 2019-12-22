c2ecf15bb71475b93973e6eb1e72e56d72ccfcbf
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2019-12-22 00:46:20 -0500

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
+	
+	
+  def after_sign_in_path_for(resource)
+    if current_user.treasurer.present?
+			treasurer_path(current_user.treasurer)
+		else
+			new_treasurer_path
+		end
+  end 
 end

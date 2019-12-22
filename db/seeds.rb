ce9a23aac19af9e316f18241352e9118c1e2ef78
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2019-12-22 00:49:32 -0500

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,13 +10,11 @@
 edu_level = ["Doctorate","Master","Primary","Secondary", "Bachelors"]
 treasurer_type = ["principal treasurer","assistant principal treasurer","head treasurer","assistant head treasurer"]
 
-15.times do
+6.times do
   user = User.create(email: Faker::Internet.email, password: "memphis")
-	treasurer = Treasurer.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, date_of_birth: Faker::Date.birthday, branch_id: (1..5).to_a.sample, council: Faker::Address.city, whatsapp_number: Faker::PhoneNumber.cell_phone_with_country_code, employment_status: true, education_level: edu_level.sample, born_again: true, ud_join: Faker::Date.between(from: 32.years.ago, to: 5.years.ago), bishop_podcast: true, year_treasurer: Faker::Date.between(from: 4.years.ago, to: Date.today), treasurer_type: treasurer_type.sample, training_manual: true, tithe: (0..2).to_a.sample, user_id: user.id)
+	treasurer = Treasurer.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, date_of_birth: Faker::Date.birthday, branch_id: (1..5).to_a.sample, council: Faker::Address.city, whatsapp_number: Faker::PhoneNumber.cell_phone_with_country_code, employment_status: true, education_level: Treasurer::EDUCATION.sample, born_again: true, ud_join: Faker::Date.between(from: 32.years.ago, to: 5.years.ago), bishop_podcast: true, year_treasurer: Faker::Date.between(from: 4.years.ago, to: Date.today), treasurer_type: Treasurer::TREASURER_TYPE.sample, training_manual: true, tithe: (0..2).to_a.sample, user_id: user.id)
 	treasurer.conference = ["2019","2018"]
 	treasurer.save
 	file = URI.open('https://picsum.photos/280/280')
 	treasurer.image.attach(io: file, filename: "#{Faker::Lorem.word}.jpg")
 end
-
-conference  

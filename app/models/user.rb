c3d82c946e982bfd87648e170bb64a63c89be93f
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2019-12-23 12:25:49 -0500

    adding SMTP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #  and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable, :lockable, :timeoutable, :trackable
 	has_one :treasurer
 	enum role: [:admin, :principal_treasurer, :treasurer]
 
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,18 @@ module Trasury
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+		config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              ENV["SMTP_HOST"],
+      port:                 ENV["SMTP_PORT"],
+      user_name:            ENV["SMTP_USERNAME"],
+      password:             ENV["SMTP_PASSWORD"],
+      authentication:       ENV["SMTP_AUTHENTICATION"],
+      domain:       ENV["SMTP_DOMAIN"],
+      openssl_verify_mode: "none"
+    }
+		
+		
+		
   end
 end

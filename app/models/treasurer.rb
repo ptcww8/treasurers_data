aa0f12b4cc8a1edc350e4779ab6e4e8283555a7e
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2019-12-30 15:50:48 -0500

    removing errors when image is not attached

--- app/models/treasurer.rb
+++ app/models/treasurer.rb
@@ -7,7 +7,7 @@ class Treasurer < ApplicationRecord
 	serialize :debt, Array
 	enum tithe: [:yes, :no, :occasionally] 
 	validates_presence_of :first_name, :last_name, :date_of_birth, :branch_id, :council, :whatsapp_number, :education_level, :ud_join, :year_treasurer, :treasurer_type, :tithe
-	validates_inclusion_of :employment_status, :bishop_podcast, :born_again, :training_manual, :in => [true, false]
+	validates_inclusion_of :employment_status, :bishop_podcast, :born_again, :in => [true, false]
 	EDUCATION = ["primary","secondary", "bachelors","master","doctorate"]
   TREASURER_TYPE = ["principal treasurer","head treasurer","assistant head treasurer"]
 	DEBT_OBLIGATION = ["mortgage","car loan","school loan", "alimony", "child support", "other(please explain)"]

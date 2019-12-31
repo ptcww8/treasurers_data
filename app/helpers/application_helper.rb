287805e5358f104c56d6250c1eb5d5233cd08b2a
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2019-12-31 09:59:43 -0500

    fix error

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def app_name
     "UDOLGC Treasurers"
   end
+	
+	def show_questionnaire
+		current_user && (current_user.treasurer && current_user.treasurer.treasurer_type && Treasurer::TREASURER_TYPE.key(current_user.treasurer.treasurer_type) &&Treasurer::TREASURER_TYPE.key(current_user.treasurer.treasurer_type).downcase.include?("head")) || current_user.admin? || current_user.principal_treasurer?
+		
+	end
 end

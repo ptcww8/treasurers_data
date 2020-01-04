f92f2a672f093d1211db667ea8e5205e767c38d5
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2020-01-04 15:49:35 -0500

    beauty

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,19 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 	config.action_mailer.default_url_options = { host: "https://railsclass1-2-princetituscodjoe899265.codeanyapp.com" }
+	
+	config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              "smtp.sendgrid.net",
+      port:                 587,
+      user_name:            "apikey",
+      password:             "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",
+      authentication:       "plain",
+      domain:       "rubyonrailstrainingstl.com",
+		  enable_starttls_auto: true
+    }
+		
+		
+	
+	
 end

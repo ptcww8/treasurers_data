1eda2bcbd8d0f712bcd12e55cb62698f88f3e74b
prince.titus.codjoe <prince.titus.codjoe@gmail.com>
2020-01-13 20:06:39 -0500

    modifications of different regions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   end
 	
 	def show_questionnaire
-		current_user && (current_user.treasurer && current_user.treasurer.treasurer_type && Treasurer::TREASURER_TYPE.key(current_user.treasurer.treasurer_type) &&Treasurer::TREASURER_TYPE.key(current_user.treasurer.treasurer_type).downcase.include?("head")) || current_user.admin? || current_user.principal_treasurer?
+		current_user && (current_user.treasurer && current_user.treasurer.treasurer_type && Treasurer::TREASURER_TYPE.key(current_user.treasurer.treasurer_type) && Treasurer::TREASURER_TYPE.key(current_user.treasurer.treasurer_type).downcase.include?("head") && current_user.treasurer.verified) || current_user.admin? || current_user.principal_treasurer?
 		
 	end
 end
